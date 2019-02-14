5a01f91e1c04fac3b8fb1887af2b2f23a4b22974
ea-q-le <sean.gadimoff@gmail.com>
2019-02-14 09:20:27 -0500

    added logic to DataDrivenTest class

--- src/test/java/excelautomation/DataDrivenTest.java
+++ src/test/java/excelautomation/DataDrivenTest.java
@@ -2,19 +2,27 @@ package excelautomation;
 
 import com.weborders.utilities.ConfigurationReader;
 import com.weborders.utilities.Driver;
+import com.weborders.utilities.ExcelUtil;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class DataDrivenTest {
     private WebDriver driver;
+    List<Map<String, String>> testData;
 
     @BeforeMethod
     public void setupDriver() {
+        ExcelUtil excelUtil = new ExcelUtil("./src/test/resources/EmployeesTestData.xlsx","data");
+        testData = excelUtil.getDataList();
+
         driver = Driver.getDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(ConfigurationReader.getProperty("data-url"));
@@ -26,17 +34,27 @@ public class DataDrivenTest {
 
     @Test
     public void employeesFormTest() {
-        EmployeesFormPage empPage = new EmployeesFormPage();
-        empPage.firstName.sendKeys("Mark");
-        empPage.lastName.sendKeys("Smith");
-        empPage.role.sendKeys("Student");
-        empPage.selectGender("male");
-        empPage.email.sendKeys("kigus@parcel4.net");
-        empPage.annualSalary.sendKeys("90123");
-        new Select(empPage.education).selectByVisibleText("High School");
-        empPage.javaOCACert.click();
-        empPage.additionalSkills.sendKeys("Google > Copy > Paste");
-        empPage.submitBttn.click();
+        for (Map<String, String> empData : testData) {
 
+            EmployeesFormPage empPage = new EmployeesFormPage();
+            empPage.firstName.sendKeys(empData.get("first_name"));
+            empPage.lastName.sendKeys(empData.get("last_name"));
+            empPage.role.sendKeys(empData.get("Role"));
+            empPage.selectGender(empData.get("gender"));
+            empPage.email.sendKeys(empData.get("email"));
+            empPage.annualSalary.sendKeys(empData.get("Salary").replace(".0",""));
+            new Select(empPage.education).selectByVisibleText(empData.get("Education"));
+            empPage.javaOCACert.click();
+            empPage.additionalSkills.sendKeys(empData.get("Skills"));
+            empPage.submitBttn.click();
+
+            ConfirmationPage cPage = new ConfirmationPage();
+
+            Assert.assertEquals(empData.get("first_name") +" "+empData.get("last_name"),
+                    cPage.getFullName());
+
+            Driver.getDriver().get(ConfigurationReader.getProperty("employees.app.url"));
+
+        }
     }
 }
